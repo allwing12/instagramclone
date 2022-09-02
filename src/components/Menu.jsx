2aad8d9972241fd181080973bc53b532275aef25
allwing12 <allwing1232@gmail.com>
2022-09-02 18:24:08 +0900

    스크롤 해결

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,22 +1,21 @@
 import "../styles/Menu.scss";
-import {ReactComponent as Home} from "../Images/home.svg";
-import {ReactComponent as Inbox} from "../Images/Inbox.svg";
-import {ReactComponent as Explore} from "../Images/Explore.svg";
-import {ReactComponent as Notifications} from "../Images/Notifications.svg";
+import { ReactComponent as Home } from "../Images/home.svg";
+import { ReactComponent as Inbox } from "../Images/Inbox.svg";
+import { ReactComponent as Explore } from "../Images/Explore.svg";
+import { ReactComponent as Notifications } from "../Images/Notifications.svg";
 import image from "../Images/profile.jpg";
 import ProfileIcon from "./ProfileIcon";
 
-
-function Menu () {
-    return(
-        <div className="menu">
-            <Home clssName="icon" />
-            <Inbox clssName="icon" />
-            <Explore clssName="icon" />
-            <Notifications clssName="icon" />
-            <ProfileIcon iconSize="small" image={image} storyBorder={true} />    
-            </div>
-    )
+function Menu() {
+  return (
+    <div className="Menu">
+      <Home clssName="icon" />
+      <Inbox clssName="icon" />
+      <Explore clssName="icon" />
+      <Notifications clssName="icon" />
+      <ProfileIcon iconSize="small" image={image} storyBorder={true} />
+    </div>
+  );
 }
 
 export default Menu;
--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -1,25 +1,35 @@
 import "../styles/Stories.scss";
-// import HorizontalScroll from "react-scroll-horizontal";
+import HorizontalScroll from "react-scroll-horizontal";
 import Story from "./Story";
 
-function Stories () {
-    return (
-        <div className = "Stories">
-            {/* <HorizontalScroll className = "scroll" reverseScroll={true}> */}
-                <Story />
-                <Story />
-                <Story />
-                <Story />
-                <Story />
-                <Story />
-                <Story />
-                <Story />
-                <Story />
-                <Story />
-                {/* </HorizontalScroll> */}
-        </div>
-
-    )
+function Stories() {
+  return (
+    <div className="Stories">
+      <HorizontalScroll className="Scroll" reverseScroll={true}>
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+      </HorizontalScroll>
+    </div>
+  );
 }
 
 export default Stories;
+/* <ScrollHorizontal className="scroll" reverseScroll={true}></ScrollHorizontal> */
